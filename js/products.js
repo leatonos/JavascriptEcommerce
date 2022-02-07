52f3fded82c819af28923485fa3b1d59e0c4797f
leatonos <pedrofmattosb@gmail.com>
2022-02-07 15:00:28 -0500

    Installed slqlite npm and created Product Class

---
+++ js/products.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(ProductID, ProductName, ProductPrice, ProductDescription, ProductType, ProductImage) {
+      this.ProductID = ProductID;
+      this.ProductName = ProductName;
+      this.ProductPrice = ProductPrice;
+      this.ProductDescription = ProductDescription;
+      this.ProductType = ProductType;
+      this.ProductImage = ProductImage;
+    }
+}
