a2bfc73782e7ee379178d0772884fe9d83c783d7
leatonos <pedrofmattosb@gmail.com>
2022-02-10 17:06:18 -0500

    some changes in the code

--- customer.js
+++ customer.js
@@ -41,13 +41,16 @@ app.post("/newRecord",function(req,res){
     }
     res.redirect(`/`);
 });
+
 app.post("/log-in",function(req,res){
     
     if (req.body) {
         logIn(req.body, res)
-        
+        res.redirect(`/`);
     }
     
+    res.redirect(`/`);
+
 });
 app.listen(3200)
 
@@ -67,25 +70,35 @@ function newRecord(customer) {
         err ? console.error(err) : console.log(`A row has been inserted.`)
     );
   });
-  //db.close();
+  db.close();
 }
+
+
+
+
 function logIn(login, res){
-  db.each("SELECT * FROM Customers WHERE CustomerEmail = '" + login.CustomerEmail + "'", function (err, row) {
-    console.log(row)
-      if(logIn.CustomerPassword = row.CustomerEmail) {
-        console.log("Login Correct")
-        res.redirect(`/home`);
-      }
-      else{
-        res.redirect(`/`);
-        
-      }
-    });
+  console.log("Login Function Result:")
+  console.log(login.CustomerEmail);
+  console.log(login.CustomerPassword);
+  db.each("SELECT * FROM Customers", function (err, row) {
+
+    if(login.CustomerEmail == row.CustomerEmail && login.CustomerPassword == row.CustomerPassword){
+
+    }
+    
+    /*
+    console.log("----------------------")
+    console.log(login.CustomerEmail == row.CustomerEmail);
+    console.log(login.CustomerEmail);
+    console.log(row.CustomerEmail);
+    */
+    
+  });
+  //db.close();
 }
 
 /**
- * 
- * db.each("SELECT * FROM Customers", function (err, row) {
+ db.each("SELECT * FROM Customers", function (err, row) {
       console.log(row);
     });
  */
